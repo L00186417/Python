41d4c3397bba96a808c571fd49ee2b5d270025ac
Rashmi <L00186417@atu.ie>
2024-03-02 00:10:58 +0000

    added example1

---
+++ example3.py
@@ -0,0 +1,6 @@
+my_string= "Good Morning everyone!"
+my_upper= my_string.upper()
+my_lower=my_string.lower()
+print(f"original: {my_string} ")
+print(f"upper: {my_upper} ")
+print(f"lower: {my_lower} ")
